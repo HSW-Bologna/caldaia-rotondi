34c2d5840495e13dfef9b297090c1a6e58d58214
maldus512 <mattia512maldini@gmail.com>
2024-07-18 12:09:03 +0200

    Added modbus functions

--- src/adapters/modbus_server.c
+++ src/adapters/modbus_server.c
@@ -2,8 +2,9 @@
 #define LIGHTMODBUS_IMPL
 #define LIGHTMODBUS_SLAVE
 #define LIGHTMODBUS_F03S
+#define LIGHTMODBUS_F04S
 #define LIGHTMODBUS_F06S
-#define LIGHTMODBUS_F016S
+#define LIGHTMODBUS_F16S
 #include <lightmodbus/lightmodbus.h>
 
 #include "services/timestamp.h"
@@ -15,34 +16,32 @@
 #include "bsp/rs485.h"
 
 enum {
-    MODBUS_HR_DEVICE_MODEL                   = 0,
-    MODBUS_HR_FIRMWARE_VERSION       ,
-    MODBUS_HR_INSTANT_ANALOG_VALUE_R1 ,
-    MODBUS_HR_INSTANT_ANALOG_VALUE_S  ,
-    MODBUS_HR_INSTANT_ANALOG_VALUE_T  ,
-    MODBUS_HR_REQUIRED_DUTY_CYCLE     ,
+    MODBUS_IR_DEVICE_MODEL = 0,
+    MODBUS_IR_FIRMWARE_VERSION,
+    MODBUS_IR_INSTANT_ANALOG_VALUE_R1,
+    MODBUS_IR_INSTANT_ANALOG_VALUE_S,
+    MODBUS_IR_INSTANT_ANALOG_VALUE_T,
 };
 
+enum {
+    MODBUS_HR_REQUIRED_DUTY_CYCLE = 0,
+};
 
 static ModbusError register_callback(const ModbusSlave *minion,
-                             const ModbusRegisterCallbackArgs *args,
-                             ModbusRegisterCallbackResult *result);
-static ModbusError static_allocator(
-    ModbusBuffer *buffer,
-    uint16_t size,
-    void *context);
-static ModbusError exception_callback(const ModbusSlave *minion, uint8_t function,
-                              ModbusExceptionCode code);
-
+                                     const ModbusRegisterCallbackArgs *args,
+                                     ModbusRegisterCallbackResult *result);
+static ModbusError static_allocator(ModbusBuffer *buffer, uint16_t size,
+                                    void *context);
+static ModbusError exception_callback(const ModbusSlave *minion,
+                                      uint8_t function,
+                                      ModbusExceptionCode code);
 
 static ModbusSlave minion;
-
 
 void modbus_server_init(void) {
     // Read the data from stdin
     ModbusErrorInfo err = modbusSlaveInit(&minion, register_callback,
-                                          exception_callback,
-                                          static_allocator,
+                                          exception_callback, static_allocator,
                                           modbusSlaveDefaultFunctions,
                                           modbusSlaveDefaultFunctionCount);
 
@@ -50,19 +49,19 @@ void modbus_server_init(void) {
     assert(modbusIsOk(err) && "modbusSlaveInit() failed!");
 }
 
-
 void modbus_server_manage(mut_model_t *p_model) {
     uint8_t request[BSP_RS485_MAX_PACKET_LEN] = { 0 };
     size_t request_length = bsp_rs485_read(request, sizeof(request));
 
     if (request_length > 0 && bsp_rs485_timed_out(50)) {
         modbusSlaveSetUserPointer(&minion, p_model);
-        ModbusErrorInfo err = modbusParseRequestRTU(&minion, 1, request, (uint8_t)request_length);
+        ModbusErrorInfo err = modbusParseRequestRTU(&minion, 1, request,
+                                                    (uint8_t) request_length);
 
         if (modbusIsOk(err)) {
             const uint8_t *response = modbusSlaveGetResponse(&minion);
             size_t response_length = modbusSlaveGetResponseLength(&minion);
-            bsp_rs485_write((uint8_t*)response, response_length);
+            bsp_rs485_write((uint8_t*) response, response_length);
             bsp_rs485_flush();
         } else {
             bsp_rs485_flush();
@@ -72,54 +71,65 @@ void modbus_server_manage(mut_model_t *p_model) {
     }
 }
 
-
-static ModbusError static_allocator(
-    ModbusBuffer *buffer,
-    uint16_t size,
-    void *context)
-{
-    (void)context;
-    #define MAX_RESPONSE 256
+static ModbusError static_allocator(ModbusBuffer *buffer, uint16_t size,
+                                    void *context) {
+    (void) context;
+#define MAX_RESPONSE 256
 
     static uint8_t response[MAX_RESPONSE];
 
     if (size != 0) // Allocation reqest
-    {
+            {
         if (size <= MAX_RESPONSE) // Allocation request is within bounds
         {
             buffer->data = response;
             return MODBUS_OK;
-        }
-        else // Allocation error
+        } else // Allocation error
         {
             buffer->data = NULL;
             return MODBUS_ERROR_ALLOC;
         }
-    }
-    else // Free request
+    } else // Free request
     {
         buffer->data = NULL;
         return MODBUS_OK;
     }
 }
 
-
 static ModbusError register_callback(const ModbusSlave *minion,
-                             const ModbusRegisterCallbackArgs *args,
-                             ModbusRegisterCallbackResult *result) {
+                                     const ModbusRegisterCallbackArgs *args,
+                                     ModbusRegisterCallbackResult *result) {
     mut_model_t *p_model = modbusSlaveGetUserPointer(minion);
 
     switch (args->query) {
         // Pretend to allow all access
         case MODBUS_REGQ_R_CHECK:
-            result->exceptionCode = MODBUS_EXCEP_NONE;
+            switch (args->type) {
+                case MODBUS_INPUT_REGISTER:
+                case MODBUS_HOLDING_REGISTER:
+                    result->exceptionCode = MODBUS_EXCEP_NONE;
+                    break;
+
+                default:
+                    result->exceptionCode = MODBUS_EXCEP_ILLEGAL_FUNCTION;
+                    break;
+            }
             break;
 
         case MODBUS_REGQ_W_CHECK:
-            switch (args->index) {
-                case MODBUS_HR_REQUIRED_DUTY_CYCLE:
-                    result->exceptionCode = MODBUS_EXCEP_NONE;
+            switch (args->type) {
+                case MODBUS_HOLDING_REGISTER:
+                    switch (args->index) {
+                        case MODBUS_HR_REQUIRED_DUTY_CYCLE:
+                            result->exceptionCode = MODBUS_EXCEP_NONE;
+                            break;
+                        default:
+                            result->exceptionCode =
+                                    MODBUS_EXCEP_ILLEGAL_ADDRESS;
+                            break;
+                    }
                     break;
+
                 default:
                     result->exceptionCode = MODBUS_EXCEP_ILLEGAL_FUNCTION;
                     break;
@@ -128,46 +138,81 @@ static ModbusError register_callback(const ModbusSlave *minion,
 
             // Return 7 when reading
         case MODBUS_REGQ_R: {
-            switch (args->index) {
-                case MODBUS_HR_DEVICE_MODEL:
-                    result->value = APP_CONFIG_DEVICE_MODEL;
-                    break;
+            switch (args->type) {
+                case MODBUS_INPUT_REGISTER: {
+                    switch (args->index) {
+                        case MODBUS_IR_DEVICE_MODEL:
+                            result->value = APP_CONFIG_DEVICE_MODEL;
+                            break;
 
-                case MODBUS_HR_FIRMWARE_VERSION:
-                    result->value = ((APP_CONFIG_FIRMWARE_VERSION_MAJOR & 0x1F) << 11) |
-                            ((APP_CONFIG_FIRMWARE_VERSION_MINOR & 0x1F) << 6) |
-                            (APP_CONFIG_FIRMWARE_VERSION_MAJOR & 0x3F);
-                    break;
+                        case MODBUS_IR_FIRMWARE_VERSION:
+                            result->value =
+                                    ((APP_CONFIG_FIRMWARE_VERSION_MAJOR & 0x1F)
+                                            << 11)
+                                            | ((APP_CONFIG_FIRMWARE_VERSION_MINOR
+                                                    & 0x1F) << 6)
+                                            | (APP_CONFIG_FIRMWARE_VERSION_MAJOR
+                                                    & 0x3F);
+                            break;
 
-                case MODBUS_HR_INSTANT_ANALOG_VALUE_R1:
-                    result->value = p_model->adc_r1;
-                    break;
+                        case MODBUS_IR_INSTANT_ANALOG_VALUE_R1:
+                            result->value = p_model->adc_r1;
+                            break;
 
-                case MODBUS_HR_INSTANT_ANALOG_VALUE_S:
-                    result->value = p_model->adc_s;
-                    break;
+                        case MODBUS_IR_INSTANT_ANALOG_VALUE_S:
+                            result->value = p_model->adc_s;
+                            break;
 
-                case MODBUS_HR_INSTANT_ANALOG_VALUE_T:
-                    result->value = p_model->adc_t;
-                    break;
+                        case MODBUS_IR_INSTANT_ANALOG_VALUE_T:
+                            result->value = p_model->adc_t;
+                            break;
 
-                case MODBUS_HR_REQUIRED_DUTY_CYCLE:
-                    result->value = ((p_model->override_duty_cycle > 0) << 15) | (p_model->overridden_duty_cycle & 0x7FFF);
+                        default:
+                            result->value = 0;
+                            break;
+                    }
                     break;
+                }
+
+                case MODBUS_HOLDING_REGISTER: {
+                    switch (args->index) {
+                        case MODBUS_HR_REQUIRED_DUTY_CYCLE:
+                            result->value =
+                                    (uint16_t) (((p_model->override_duty_cycle
+                                            > 0) << 15)
+                                            | (p_model->overridden_duty_cycle
+                                                    & 0x7FFF));
+                            break;
+
+                        default:
+                            result->value = 0;
+                            break;
+                    }
+                    break;
+                }
 
                 default:
-                    result->value = 0;
                     break;
             }
             break;
         }
 
         case MODBUS_REGQ_W: {
-            switch (args->index) {
-                case MODBUS_HR_REQUIRED_DUTY_CYCLE:
-                    p_model->override_duty_cycle = (args->value & (1 << 15)) > 0;
-                    p_model->overridden_duty_cycle = args->value & 0x7FFF;
+            switch (args->type) {
+                case MODBUS_HOLDING_REGISTER: {
+                    switch (args->index) {
+                        case MODBUS_HR_REQUIRED_DUTY_CYCLE:
+                            p_model->override_duty_cycle = (args->value
+                                    & (1 << 15)) > 0;
+                            p_model->overridden_duty_cycle =
+                                    (uint8_t) (args->value & 0xFF);
+                            break;
+
+                        default:
+                            break;
+                    }
                     break;
+                }
 
                 default:
                     break;
@@ -182,11 +227,11 @@ static ModbusError register_callback(const ModbusSlave *minion,
     return MODBUS_OK;
 }
 
-
-static ModbusError exception_callback(const ModbusSlave *minion, uint8_t function,
-                              ModbusExceptionCode code) {
-    (void)minion;
-    (void)function;
-    (void)code;
+static ModbusError exception_callback(const ModbusSlave *minion,
+                                      uint8_t function,
+                                      ModbusExceptionCode code) {
+    (void) minion;
+    (void) function;
+    (void) code;
     return MODBUS_OK;
 }
